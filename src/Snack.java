4595833fef3dde155d1eea407ee08b663ed33b5a
AndrewLabbe <ablabbe@quinnipiac.edu>
2022-02-19 15:59:35 -0500

    Starter code, Snack class and greeting introduced

---
+++ src/Snack.java
@@ -0,0 +1,28 @@
+
+public class Snack {
+	
+	private String Name;
+	private double Price;
+	
+	public Snack(String name, double price) {
+		Name = name;
+		Price = price;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public double getPrice() {
+		return Price;
+	}
+
+	public void setPrice(double price) {
+		Price = price;
+	}
+	
+}
---
+++ src/VendingMachine.java
@@ -0,0 +1,15 @@
+
+public class VendingMachine {
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to the virtual vending machine! Listed below are your options for what snack you would like. Please enter in the number assoiciated with the snack.");
+		
+		Snack snack1 = new Snack("Lays Original", 2.00);
+		Snack snack2 = new Snack("BBQ Fritos", 1.50);
+		Snack snack3 = new Snack("Veggie Straws", 1.80);
+		Snack snack4 = new Snack("Sour Patch Kids", 2.20);
+		Snack snack5 = new Snack("Twix Bar", 2.00);
+		Snack snack6 = new Snack("Nutri-Grain Bar", 2.00);
+		
+	}
+}
